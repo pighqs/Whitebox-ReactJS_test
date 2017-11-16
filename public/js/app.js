4c9b596314486c66ddb4a168d0883739ad9f3e68
pighqs <peggy.hqs@gmail.com>
2017-11-16 08:57:58 +0100

    add onclick on carousel

--- public/js/app.js
+++ public/js/app.js
@@ -7,11 +7,11 @@ class App extends React.Component {
     render() {
         // App va retourner la "balise" <ListeBurger/>
         return (
-         <div>
-         <Header/>
-         <Home/>
-         </div>
-         );
+           <div>
+           <Header/>
+           <Home/>
+           </div>
+           );
     }
 }
 
@@ -47,8 +47,8 @@ class Home extends React.Component {
         // App va retourner la "balise" <ListeBurger/>
         return (
             <section id="home" className="padbot0">
-                <Slider/>
-                <Carousel/>
+            <Slider/>
+            <Carousel/>
             </section>
             );
     }
@@ -63,7 +63,7 @@ class Slider extends React.Component {
     render() {
         const captionDatas = [
         {liClass:"slide1 flex-active-slide", classPar1: "title1 captionDelay2 FromTop", classPar2:"title2 captionDelay4 FromTop", classPar3:"title3 captionDelay6 FromTop", classPar4:"title4 captionDelay7 FromTop", city: "Lyon", title:"vacances été", year: "2017" , desc: "Vacances au calme dans une petite maison de campagne" },
-        {liClass:"slide2", classPar1: "title1 captionDelay6 FromLeft", classPar2:"title2 captionDelay4 FromLeft", classPar3:"title3 captionDelay2 FromLeft", classPar4:"title4 captionDelay7 FromLeft", city: "Bretagne", title:"weekend de mai", year: "2016" , desc: "Un moment de detente en famille" },
+        {liClass:"slide2", classPar1: "title1 captionDelay6 FromLeft", classPar2:"title2 captionDelay4 FromLeft", classPar3:"title3 captionDelay2 FromLeft", classPar4:"title4 captionDelay7 FromLeft", city: "Sarcelles", title:"weekend de mai", year: "2016" , desc: "Un moment de detente en famille" },
         {liClass:"slide3", classPar1: "title1 captionDelay1 FromBottom", classPar2:"title2 captionDelay2 FromBottom", classPar3:"title3 captionDelay3 FromBottom", classPar4:"title4 captionDelay5 FromBottom", city: "Biarritz", title:"Anniversaire Allan", year: "2016" , desc: "Surf and fun entre amis" }
         ];
 
@@ -75,12 +75,12 @@ class Slider extends React.Component {
         }
 
         return (
-           <div className="flexslider top_slider">
-                <ul className="slides">
-                        {slides}
-                </ul>
-           </div>
-           );
+         <div className="flexslider top_slider">
+         <ul className="slides">
+         {slides}
+         </ul>
+         </div>
+         );
     }
 }
 
@@ -93,12 +93,12 @@ class Slide extends React.Component {
     render() {
         return (
             <li className={this.props.liClass}>
-                <div className="flex_caption1">
-                    <p className={this.props.classPar1}>{this.props.city}</p>
-                    <p className={this.props.classPar2}>{this.props.title}</p>
-                    <p className={this.props.classPar3}>{this.props.year}</p>
-                    <p className={this.props.classPar4}>{this.props.desc}</p>
-                </div>
+            <div className="flex_caption1">
+            <p className={this.props.classPar1}>{this.props.city}</p>
+            <p className={this.props.classPar2}>{this.props.title}</p>
+            <p className={this.props.classPar3}>{this.props.year}</p>
+            <p className={this.props.classPar4}>{this.props.desc}</p>
+            </div>
             </li>
             );
     }
@@ -113,25 +113,25 @@ class Carousel extends React.Component {
     }
 
     render() {
-         const carouselDatas = [
-            { target:"slide1", image: "images/slider/slide1_bg.jpg", classN:"flex-active-slide"},
-            { target:"slide2", image: "images/slider/slide2_bg.jpg", classN:""},
-            { target:"slide3", image: "images/slider/slide3_bg.jpg", classN:""},
-            ];
+       const carouselDatas = [
+       { target:"slide1", image: "images/slider/slide1_bg.jpg"},
+       { target:"slide2", image: "images/slider/slide2_bg.jpg"},
+       { target:"slide3", image: "images/slider/slide3_bg.jpg"},
+       ];
 
-        let carouselList = [];
+       let carouselList = [];
 
-        for (var i = 0; i < carouselDatas.length; i++) {
-            carouselList.push(<CarouselSlide key={"carouselSlides_"+i} target={carouselDatas[i].target} image={carouselDatas[i].image} classN={carouselDatas[i].classN}/>);
+       for (var i = 0; i < carouselDatas.length; i++) {
+        carouselList.push(<CarouselSlide key={"carouselSlides_"+i} target={carouselDatas[i].target} image={carouselDatas[i].image}/>);
         }
 
-        return (
-            <div id="carousel">
-                <ul className="slides">
-                    {carouselList}
-                </ul>
-            </div>
-         );
+    return (
+        <div id="carousel">
+        <ul className="slides">
+        {carouselList}
+        </ul>
+        </div>
+        );
     }
 }
 
@@ -139,20 +139,44 @@ class CarouselSlide extends React.Component {
 
     constructor() {
         super();
-    }
 
-    render() {
-        return (
-            <li data-target={this.props.target} className={this.props.classN}>
-                <img src={this.props.image} alt="" />
-            </li>
-            );
+        //Toujours "binder" la fonction de l'événement :  ici le this fait référence à CarouselSlide, le bind sert a indiquer la valeur du this
+        this.handleClick = this.handleClick.bind(this);
+        // creation d'un état, nom "flexActive", valeur "false"
+        this.state = { classActive: false };
+   }
+
+
+   handleClick() {
+      this.setState({
+        classActive: !this.state.flexActive
+    });
+  }
+
+
+  render() {
+
+    var classN;
+    if(this.state.classActive === true) {
+        classN = "flex-active-slide";
+   } else {
+        classN = "";
+   }
+
+    return (
+        <li
+        onClick={this.handleClick}
+        data-target={this.props.target}
+        className={classN}>
+        <img src={this.props.image} alt="" />
+        </li>
+        );
     }
 }
 
 
 ReactDOM.render(
-   <App/>
-   ,
-   document.getElementById('page')
-   );
+ <App/>
+ ,
+ document.getElementById('page')
+ );
